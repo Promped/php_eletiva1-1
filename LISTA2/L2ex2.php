26b487ad031bdeb4e167a555efc4b4c4735ca85f
Larissa Carolina <larissa19999oliveira@gmail.com>
2025-03-03 15:13:28 -0300

    Update L2ex2.php

--- LISTA2/L2ex2.php
+++ LISTA2/L2ex2.php
@@ -5,6 +5,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <body>
+
+    
+    <form method="post">
+                        
+                        <div class="mb-3">
+                            <label for="valor1" class="form-label">Insira o primeiro valor</label>
+                            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="valor2" class="form-label">Insira o segundo valor</label>
+                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
+                        </div>
+                    
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            
+
 
 
 
