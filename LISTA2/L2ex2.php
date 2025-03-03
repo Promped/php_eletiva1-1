b9bfefa32f85b612256f8dbfee2fcc207e3b5c2c
Larissa Carolina <larissa19999oliveira@gmail.com>
2025-03-03 15:17:33 -0300

    Update L2ex2.php

--- LISTA2/L2ex2.php
+++ LISTA2/L2ex2.php
@@ -4,37 +4,32 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 2</title>
+    <link href=""  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
 
     <body>
 
     
-    <form method="post">
-                        
-                        <div class="mb-3">
-                            <label for="valor1" class="form-label">Insira o primeiro valor</label>
-                            <input type="number" id="valor1" name="valor1" class="form-control" required="">
-                        </div>
-                    
-                        <div class="mb-3">
-                            <label for="valor2" class="form-label">Insira o segundo valor</label>
-                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
-                        </div>
-                    
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </form>
-            
+        <form method="post">
+                            
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Insira o primeiro valor</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+        
+            <div class="mb-3">
+                <label for="valor2" class="form-label">Insira o segundo valor</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+        
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
 
 
-
-
-
-
-
-<?php
-
-
-
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
 
 
 
@@ -43,4 +38,15 @@
 
 
 
-?>
+
+
+
+
+
+
+
+
+
+
+
+
