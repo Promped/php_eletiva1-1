8c99aad20cfaba749587fe470dee2808677f0eef
Larissa Carolina <larissa19999oliveira@gmail.com>
2025-03-03 15:20:00 -0300

    Update L2ex2.php

--- LISTA2/L2ex2.php
+++ LISTA2/L2ex2.php
@@ -5,12 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Exercício 2</title>
-    <link href=""  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 
     <body>
 
-    
+    <form method="post" action="L2resp2.php">
         <form method="post">
                             
             <div class="mb-3">
