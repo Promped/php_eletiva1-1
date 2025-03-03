61c89994471c5e5e71238a3b453b9b22ee88ff35
Larissa Carolina <larissa19999oliveira@gmail.com>
2025-03-03 16:16:20 -0300

    L2exercicio4

--- LISTA2/L2ex3.php
+++ LISTA2/L2ex3.php
@@ -4,12 +4,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 2</title>
+    <title>Exercício 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 
     <body>
-        <h1>Exercício 2</h1>
+        <h1>Exercício 3</h1>
         <form method="post" action="L2resp3.php">
                            
             <div class="mb-3">
