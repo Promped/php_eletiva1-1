c397654e734835bd5435b93c32df5a3ccedcb044
Larissa Carolina <larissa19999oliveira@gmail.com>
2025-03-03 15:54:56 -0300

    Update L2ex2.php

--- LISTA2/L2ex2.php
+++ LISTA2/L2ex2.php
@@ -9,17 +9,16 @@
   </head>
 
     <body>
-
-    <form method="post" action="L2resp2.php">
-        <form method="post">
-                            
+        <h1>Exercício 2</h1>
+        <form method="post" action="L2resp2.php">
+                           
             <div class="mb-3">
-                <label for="valor1" class="form-label">Insira o primeiro valor</label>
+                <label for="valor1" class="form-label">Insira o primeiro valor: </label>
                 <input type="number" id="valor1" name="valor1" class="form-control" required="">
             </div>
         
             <div class="mb-3">
-                <label for="valor2" class="form-label">Insira o segundo valor</label>
+                <label for="valor2" class="form-label">Insira o segundo valor: </label>
                 <input type="number" id="valor2" name="valor2" class="form-control" required="">
             </div>
         
